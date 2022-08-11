51df0bd1037aa17df0bd3174c2ae167b6ac691d7
Björn Hommel <bjorn.hommel@gmail.com>
2022-08-11 22:59:16 +0200

    init commit

---
+++ magnolia-pm-website/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from "@/components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <TheWelcome />
+  </main>
+</template>
