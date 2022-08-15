6f523411265d67eaa7b5f32a50b13ebde35a72ed
Björn Hommel <bjorn.hommel@gmail.com>
2022-08-15 13:44:13 +0200

    cowboy commit pang pang

--- magnolia-pm-website/.eslintrc.cjs
+++ magnolia-pm-website/.eslintrc.cjs
@@ -3,10 +3,12 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/eslint-config-prettier"],
+  "prettier/prettier": [
+    "error",
+    {
+      endOfLine: "auto",
+    },
   ],
   parserOptions: {
     ecmaVersion: "latest",
--- magnolia-pm-website/tailwind.config.js
+++ magnolia-pm-website/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  variants: {
+    extend: {
+      display: ["group-hover"],
+    },
+  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "magnolia-blue": "#324e99",
+        "magnolia-highlight": "#1850E7",
+      },
+      fontFamily: {
+        sans: ["Source Sans Pro", "sans-serif"],
+        material: ["Material Symbols Sharp"],
+      },
+    },
   },
   plugins: [],
 };
